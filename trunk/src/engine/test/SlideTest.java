4c7d107db6145fcd8b1a78f3dbcbd1c1ac1ab7c4
jeffery.ahern <jeffery.ahern@244bd6f6-c7de-11de-a9c8-2fbcfba63733>
2009-11-03 18:35:18 +0000

    Add outline of Engine

---
+++ trunk/src/engine/test/SlideTest.java
@@ -0,0 +1,60 @@
+package engine.test;
+
+import engine.raw.Body;
+import engine.raw.SlideJoint;
+import engine.raw.StaticBody;
+import engine.raw.World;
+import engine.raw.shapes.Circle;
+import engine.vector.Vector2f;
+
+/**
+ * A test to demonstrate the SlidingJoint - wierd results! :)
+ * 
+ * @author guRuQu
+ */
+public class SlideTest extends AbstractDemo {
+	
+	/**
+	 * Create a new test
+	 */
+	public SlideTest(){
+		super("Slider Test");
+	}
+	
+	/**
+	 * @see engine.test.AbstractDemo#init(engine.raw.World)
+	 */
+	protected void init(World world) {
+		Body knot = new StaticBody(new Circle(10));
+		knot.setPosition(250,100);
+		world.add(knot);
+		
+		int N=5;
+		Body bodies[] = new Body[N];
+		for(int i=0;i<N;i++){
+			Body ball = new Body(new Circle(5),10);
+			ball.setDamping(0.01f);
+			ball.setPosition(280+i*30,100);
+			world.add(ball);
+			bodies[i]=ball;
+		}
+		
+		for(int i=1;i<N;i++){
+			SlideJoint sj = new SlideJoint(bodies[i-1],bodies[i],new Vector2f(),new Vector2f(),40,80,1f);
+			world.add(sj);
+		}
+		SlideJoint sj = new SlideJoint(knot,bodies[0],new Vector2f(),new Vector2f(),10,80,1f);
+		world.add(sj);
+	}
+	
+
+	/**
+	 * Entry point to the test
+	 * 
+	 * @param args The arguments passed to the test
+	 */
+	public static void main(String[] args) {
+		(new SlideTest()).start();
+	}
+
+}
