3292762a4ad9ad202f1a5eb35eb5132c197e7db7
nfaction <nfaction@244bd6f6-c7de-11de-a9c8-2fbcfba63733>
2009-11-10 22:40:58 +0000

    Gui setup.  Nothing major.  Setup of class MainGUI

---
+++ trunk/src/graphics/MainGUI.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package graphics;
+
+/**
+ * @author nfaction
+ *
+ */
+public class MainGUI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
