1e2222d562b155268807dc1786c7c9a1faac373b
nfaction <nfaction@244bd6f6-c7de-11de-a9c8-2fbcfba63733>
2009-11-16 20:17:43 +0000

    Beginning work on Sandbox

--- trunk/src/graphics/SandboxPanel.java
+++ trunk/src/graphics/SandboxPanel.java
@@ -1,7 +1,14 @@
 package graphics;
 
+import java.awt.Color;
+
 import javax.swing.JPanel;
 
 public class SandboxPanel extends JPanel{
-	
+	public SandboxPanel(){
+		this.setLayout(null);
+		this.setSize(950, 550);
+		this.setLocation(0, 50);
+		this.setBackground(Color.BLUE);
+	}
 }
