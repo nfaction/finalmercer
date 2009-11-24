fa68ef3aa7a6967f15332815bedfce935f682b6d
nfaction <nfaction@244bd6f6-c7de-11de-a9c8-2fbcfba63733>
2009-11-24 10:16:37 +0000

    Setup started for sprite sheets and sandbox frame

--- trunk/src/graphics/SandboxPanel.java
+++ trunk/src/graphics/SandboxPanel.java
@@ -1,12 +1,17 @@
 package graphics;
 
+
+import java.awt.BorderLayout;
 import java.awt.Color;
 
 import javax.swing.JPanel;
 
 public class SandboxPanel extends JPanel{
+	
+	private SpriteSheet cardImages;
+	
 	public SandboxPanel(){
-		this.setLayout(null);
+		this.setLayout(new BorderLayout());
 		this.setSize(950, 550);
 		this.setLocation(0, 50);
 		this.setBackground(Color.BLUE);
