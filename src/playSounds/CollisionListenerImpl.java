d98604564b1568113bf4c02be23dca248d03ab49
nfaction <nfaction@244bd6f6-c7de-11de-a9c8-2fbcfba63733>
2009-12-03 00:52:49 +0000

    added sounds

--- src/playSounds/CollisionListenerImpl.java
+++ src/playSounds/CollisionListenerImpl.java
@@ -11,11 +11,33 @@ public class CollisionListenerImpl implements CollisionListener{
 
 	public void collisionOccured(CollisionEvent event) {
 		String baseDir = System.getProperty("user.dir")	+ "/sounds/";
+		
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 
 		
-		//if (event.getBodyA().getName().equals(EType.pingPongBall) || 
-		//	event.getBodyB().getName().equals(EType.pingPongBall))
-		mySoundPlayer.play(baseDir + "PingPong.wav");
+		if (event.getBodyA().getName().equals("BasketBall") || 
+			event.getBodyB().getName().equals("BasketBall"))
+		mySoundPlayer.play(baseDir + "Tap.wav");
+		
+		if (event.getBodyA().getName().equals("BowlingBall") || 
+				event.getBodyB().getName().equals("BowlingBall"))
+			mySoundPlayer.play(baseDir + "BowlingBall.wav");
+		
+		if (event.getBodyA().getName().equals("Domino") || 
+				event.getBodyB().getName().equals("Domino"))
+			mySoundPlayer.play(baseDir + "TicTac.wav");
+		
+		if (event.getBodyA().getName().equals("PingPongBall") || 
+				event.getBodyB().getName().equals("PingPongBall"))
+			mySoundPlayer.play(baseDir + "Tink.wav");
+		
+		
+		
+		
+		
+			
+		
+		
 		
 
 	}
