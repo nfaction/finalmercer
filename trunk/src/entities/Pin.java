189bb85daae01f9365a5e1e614752b892b1195ca
KevinMcO@arizona.edu <KevinMcO@arizona.edu@244bd6f6-c7de-11de-a9c8-2fbcfba63733>
2009-12-04 23:07:37 +0000

    added popped balloon sound to play

---
+++ trunk/src/entities/Pin.java
@@ -0,0 +1,43 @@
+package entities;
+
+import engine.World;
+import enums.EType;
+
+public class Pin extends Entities {
+
+	public Pin(String objType) {
+		super(EType.basketball);
+
+	}
+
+	@Override
+	public void addObj(World world, float x, float y) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public float getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void removeObj(World world) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void upDate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
