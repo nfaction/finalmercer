c371c090921a13f56ac50ae13736eafb8823fcb5
KevinMcO@arizona.edu <KevinMcO@arizona.edu@244bd6f6-c7de-11de-a9c8-2fbcfba63733>
2009-12-04 20:20:18 +0000

    fixed error not new world but world.createworld

--- trunk/src/engine/test/AbstractDemo.java
+++ trunk/src/engine/test/AbstractDemo.java
@@ -36,7 +36,7 @@ public abstract class AbstractDemo {
 	/** The title of the current demo */
 	protected String title;
 	/** The world containing the physics model */
-	protected World world = new World(new Vector(0.0f, 10.0f), 10, new QuadSpaceStrategy(20,5));
+	public World world = World.createWorld(new Vector(0.0f, 10.0f), 10, new QuadSpaceStrategy(20,5));
 	/** True if the simulation is running */
 	private boolean running = true;
 	/** The rendering strategy */
