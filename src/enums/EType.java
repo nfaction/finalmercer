289d1f1f483954114f7cf3d4633879130b0ca0ff
jeffery.ahern <jeffery.ahern@244bd6f6-c7de-11de-a9c8-2fbcfba63733>
2009-12-06 05:18:55 +0000

    Added convayerbelt to ETypes

--- src/enums/EType.java
+++ src/enums/EType.java
@@ -2,6 +2,5 @@ package enums;
 
 public enum EType {
 
-	basketball, bowlingball, balloon, bucket, candle, domino, pingPongBall, rightRamp, leftRamp, light, straightRamp;
-
+	conveyorBelt, basketball, bowlingball, balloon, bucket, candle, domino, pingPongBall, rightRamp, leftRamp, light, straightRamp;
 }
