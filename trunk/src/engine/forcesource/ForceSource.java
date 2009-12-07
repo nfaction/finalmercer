50836f1f8829c84a02a63b21bd001432724fb8cb
jeffery.ahern <jeffery.ahern@244bd6f6-c7de-11de-a9c8-2fbcfba63733>
2009-12-07 18:31:45 +0000

    Minor editing on forcesource. just changed the layout. nothing differnt. Possibly in the future, will take out. still unknown

--- trunk/src/engine/forcesource/ForceSource.java
+++ trunk/src/engine/forcesource/ForceSource.java
@@ -11,8 +11,10 @@ public interface ForceSource {
 	/**
 	 * Apply the force of this source to the given body
 	 * 
-	 * @param body The body to which the force should be applied
-	 * @param dt The change in time since last update - in most cases constant
+	 * @param body
+	 *            The body to which the force should be applied
+	 * @param dt
+	 *            The change in time since last update - in most cases constant
 	 */
 	public void apply(Body body, float dt);
 }
