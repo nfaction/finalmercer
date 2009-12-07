ae6d1adfb9147b4814d851a4195c23784830f3c9
jeffery.ahern <jeffery.ahern@244bd6f6-c7de-11de-a9c8-2fbcfba63733>
2009-12-07 18:31:45 +0000

    Minor editing on forcesource. just changed the layout. nothing differnt. Possibly in the future, will take out. still unknown

--- src/engine/forcesource/WindSource.java
+++ src/engine/forcesource/WindSource.java
@@ -9,21 +9,23 @@ import engine.vector.Vector;
  * @author Jeffery D. AHern
  */
 public class WindSource implements ForceSource {
-	/** The force to be applied */
 	private Vector force = new Vector();
-	
-	/** 
+
+	/**
 	 * Create a new source
 	 * 
-	 * @param x The x component of the direction
-	 * @param y The y component of the direction
-	 * @param power The power of the window
+	 * @param x
+	 *            The x component of the direction
+	 * @param y
+	 *            The y component of the direction
+	 * @param power
+	 *            The power of the window
 	 */
 	public WindSource(float x, float y, float power) {
 		force.x = x * power;
 		force.y = y * power;
 	}
-	
+
 	/**
 	 * @see engine.forcesource.ForceSource#apply(engine.shapes.Body, float)
 	 */
