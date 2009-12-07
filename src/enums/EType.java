12940ad9958049ecf0327897f912a40bad30249c
cwafavre <cwafavre@244bd6f6-c7de-11de-a9c8-2fbcfba63733>
2009-12-07 22:30:11 +0000

    Added battery to enums

--- src/enums/EType.java
+++ src/enums/EType.java
@@ -2,5 +2,5 @@ package enums;
 
 public enum EType {
 
-	conveyorBelt, basketball, bowlingball, balloon, bucket, candle, domino, pingPongBall, rightRamp, leftRamp, light, straightRamp, gear;
+	conveyorBelt, basketball, bowlingball, balloon, bucket, candle, domino, pingPongBall, rightRamp, leftRamp, light, straightRamp, gear, battery;
 }
