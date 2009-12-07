36eb203922715cfe015844caa0e243214a055d6a
jeffery.ahern <jeffery.ahern@244bd6f6-c7de-11de-a9c8-2fbcfba63733>
2009-12-07 18:58:05 +0000

    Fixed Gear

--- src/entities/Gear.java
+++ src/entities/Gear.java
@@ -1,17 +1,5 @@
 package entities;
 
-import engine.World;
-import engine.joint.AngleJoint;
-import engine.joint.BasicJoint;
-import engine.joint.FixedJoint;
-import engine.joint.Joint;
-import engine.shapes.Body;
-import engine.shapes.Box;
-import engine.shapes.Circle;
-import engine.shapes.StaticBody;
-import engine.vector.Vector;
-import enums.EType;
-
 public class Gear extends AbstractGear {
 	public Gear() {
 		super(3);
