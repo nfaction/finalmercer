a16e3face67d4b8ac26467a5af0f6f42d85957e7
jeffery.ahern <jeffery.ahern@244bd6f6-c7de-11de-a9c8-2fbcfba63733>
2009-12-08 19:51:19 +0000

    Use Ballon or BasketBall as an idea of how to set up entities

--- src/entities/Balloon.java
+++ src/entities/Balloon.java
@@ -9,35 +9,26 @@ import enums.EType;
 public class Balloon extends Entities {
 
 	private Body balloon;
-
-//////////Info for sprite sheet/////////////////////
-	public static final int balloonWidth = 45;
-	public static final int balloonHeight = 45;
 	public int bbX = 0;
 	public int bbY = 0;
-	////////////////////////////////////////////////////
 	
 	public Balloon() {
-		super(EType.balloon);
+		super(EType.balloon,"Images/Balloon.gif", "Images/balloonSpriteSheet.png", 25, 25, 45, 45);
 		balloon = new Body("Balloon", new Circle(15.0f), .5f);
 		balloon.setPosition(200.0f, 300.0f);
 		balloon.setGravityEffected(false);
-		setImagePath("Images/Balloon.gif");
-		setSpritePath("Images/balloonSpriteSheet.png");
-		Y_LENGTH = 25;
-		X_LENGTH = 25;
 	}
 
 	@Override
 	public void addObj(World world, float x, float y) {
-		
+
 		balloon.setPosition(x, y);
 		world.add(balloon);
 	}
 
 	@Override
 	public float getX() {
-		
+
 		return balloon.getPosition().getX();
 	}
 
@@ -55,12 +46,12 @@ public class Balloon extends Entities {
 
 	@Override
 	public void upDate() {
+		super.upDate();
 		balloon.adjustVelocity(new Vector(0.0f, -1.0f));
 	}
 
 	@Override
 	public int getSpriteX() {
-		// TODO Auto-generated method stub
 		return bbX;
 	}
 
@@ -71,33 +62,13 @@ public class Balloon extends Entities {
 	}
 
 	@Override
-	public String toString() {
-
-		return "balloon";
-	}
-
-	@Override
 	public int gettouchingBodies() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getSpriteHeight() {
-
-		return balloonHeight;
-	}
-
-	@Override
-	public int getSpriteWidth() {
-
-		return balloonWidth;
+		return this.balloon.getTouchingCount();
 	}
 
 	@Override
 	public void setSprite() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 }
