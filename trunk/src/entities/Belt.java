0b6814e359001cf4abf603a16c6bb47d542839ac
KevinMcO@arizona.edu <KevinMcO@arizona.edu@244bd6f6-c7de-11de-a9c8-2fbcfba63733>
2009-12-08 17:36:19 +0000

    fixed

--- trunk/src/entities/Belt.java
+++ trunk/src/entities/Belt.java
@@ -1,36 +1,79 @@
-//package entities;
-//
-//import engine.World;
-//
-//public class Belt extends Entities {
-//
-//	public Belt(String objType) {
-//		super(objType);
-//		// TODO Auto-generated constructor stub
-//	}
-//
-//	@Override
-//	public void addObj(World world, float x, float y) {
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//	@Override
-//	public float getX() {
-//		// TODO Auto-generated method stub
-//		return 0;
-//	}
-//
-//	@Override
-//	public float getY() {
-//		// TODO Auto-generated method stub
-//		return 0;
-//	}
-//
-//	@Override
-//	public void removeObj(World world) {
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//}
+package entities;
+
+import engine.World;
+import entities.Entities;
+import enums.EType;
+
+public class Belt extends Entities {
+	
+	public Belt(){
+		super(EType.belt);
+	}
+
+	@Override
+	public void addObj(World world, float x, float y) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getSpriteHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getSpriteWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getSpriteX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getSpriteY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int gettouchingBodies() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void removeObj(World world) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setSprite() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
