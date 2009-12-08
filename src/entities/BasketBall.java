bfc38098c3bbf51a7336e6df831861f6279d6621
jeffery.ahern <jeffery.ahern@244bd6f6-c7de-11de-a9c8-2fbcfba63733>
2009-12-08 00:43:09 +0000

    Basketball good to go!!!

--- src/entities/BasketBall.java
+++ src/entities/BasketBall.java
@@ -8,28 +8,23 @@ public class BasketBall extends Entities {
 
 	private Body bBall;
 
-	public static final int Y_LENGTH = 24;
-	public static final int X_LENGTH = 24;
-
-	public int curState = 0;
-	public int dir = 0;
-	public int count = 0;
-
 	// ///////Info for sprite sheet////////////////////
 	public static final int basketBallWidth = 45;
 	public static final int basketBallHeight = 45;
-	public static int bbX = 0;
-	public static int bbY = 0;
-	public int state = 0;
-
+	public static final int Y_LENGTH = 24;
+	public static final int X_LENGTH = 24;
 	// /////////////////////////////////////////////////
-
+	
+	public int bbX = 0;
+	public int bbY = 0;
+	
 	public BasketBall() {
 		super(EType.basketball);
 		bBall = new Body("BasketBall", new Circle(20.0f), 2.0f);
 		bBall.setRestitution(1.0f);
 		bBall.setDamping(.001f);
 		bBall.setCanRest(true);
+		setImagePath("Images/BasketBallSpriteSheet.png");
 	}
 
 	public int getXLength() {
@@ -40,40 +35,19 @@ public class BasketBall extends Entities {
 		return Y_LENGTH;
 	}
 
-	/**
-	 * @return width of basketball
-	 */
 	public int getSpriteWidth() {
-
 		return basketBallWidth;
 	}
 
-	/**
-	 * @return height of basketball
-	 */
 	public int getSpriteHeight() {
 
 		return basketBallHeight;
 	}
 
-	/**
-	 * @return starting X point for sprite state
-	 */
-	public int getSpriteX(int c) {
-						
-		if(count > 20)
-			count = 0;
-		
-		if (count == 20)
-			setSprite();
-		
-		count++;
+	public int getSpriteX() {
 		return bbX;
 	}
 
-	/**
-	 * @return starting Y point for sprite state
-	 */
 	public int getSpriteY() {
 		return bbY;
 	}
@@ -84,33 +58,33 @@ public class BasketBall extends Entities {
 		rotation = rotation % (2f*(new Float(Math.PI)));
 
 		if (rotation >= 0f && rotation < 1.046f)
-			curState = 0;
+			state = 0;
 		else if (rotation >= 1.046f && rotation < 2.093)
-			curState = 1;
+			state = 1;
 		else if (rotation >= 2.093 && rotation < 3.14)
-			curState = 2;
+			state = 2;
 		else if (rotation >= 3.144 && rotation < 4.190)
-			curState = 3;
+			state = 3;
 		else if (rotation >= 4.190 && rotation < 5.236f)
-			curState = 2;
+			state = 2;
 		else if (rotation >= 5.236f && rotation < 6.3f)
-			curState = 3;
+			state = 3;
 		else if (rotation <= 0f && rotation > -1.046f)
-			curState = 3;
+			state = 3;
 		else if (rotation <= -1.046f && rotation > -2.093)
-			curState = 2;
+			state = 2;
 		else if (rotation <= -2.093 && rotation > -3.14)
-			curState = 1;
+			state = 1;
 		else if (rotation <= -3.144 && rotation > -4.190)
-			curState = 0;
+			state = 0;
 		else if (rotation <= -4.190 && rotation > -5.236f)
-			curState = 3;
+			state = 3;
 		else if (rotation <= -5.236f && rotation > -6.3f)
-			curState = 2;
+			state = 2;
 
 		System.out.println(rotation);
 
-		bbX = ((curState * 50));
+		bbX = ((state * 50));
 	}
 
 	@Override
@@ -137,16 +111,8 @@ public class BasketBall extends Entities {
 		world.remove(bBall);
 	}
 
-	@Override
-	public void upDate() {
-
-		setImageLocations();
-
-	}
-
 	public String toString() {
 		return "basketball";
-		// should we use bBall.getName() instead for consistency ?????
 	}
 
 	@Override
