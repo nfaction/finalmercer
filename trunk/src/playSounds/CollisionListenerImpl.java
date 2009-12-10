58e134084911a0872f1a383d84b32a7050c19c22
nfaction <nfaction@244bd6f6-c7de-11de-a9c8-2fbcfba63733>
2009-12-10 03:41:36 +0000

    removed sounds and moved them to entities

--- trunk/src/playSounds/CollisionListenerImpl.java
+++ trunk/src/playSounds/CollisionListenerImpl.java
@@ -16,39 +16,11 @@ public class CollisionListenerImpl implements CollisionListener{
 
 	public void collisionOccured(CollisionEvent event) {
 		String baseDir = System.getProperty("user.dir")	+ "/sounds/";
-	
-		
-		
-		if (event.getBodyA().getName().equals("BasketBall") || 
-			event.getBodyB().getName().equals("BasketBall"))
-		mySoundPlayer.play(baseDir + "Tap.wav");
-		
-		if (event.getBodyA().getName().equals("BowlingBall") || 
-				event.getBodyB().getName().equals("BowlingBall"))
-			mySoundPlayer.play(baseDir + "BowlingBall.wav");
-		
-		if (event.getBodyA().getName().equals("PingPongBall") || 
-				event.getBodyB().getName().equals("PingPongBall"))
-			mySoundPlayer.play(baseDir + "Tink.wav");
-		
-		if (event.getBodyA().getName().equals("Balloon") && 
-				event.getBodyB().getName().equals("tack")){
-			System.out.println("POP");
-			mySoundPlayer.play(baseDir + "PingPong.wav");
-			event.getBodyA().setMoveable(false);
-			//model.getWorld().remove(event.getBodyA());
-			//model.removeObjFromBoardAtLocated((int) event.getBodyA().getPosition().getX(), (int) event.getBodyA().getPosition().getY());
-			
-		}
 		
 		if (event.getBodyA().getName().equals("tack") && 
 				event.getBodyB().getName().equals("Balloon")){
-			System.out.println("POP");
 			mySoundPlayer.play(baseDir + "PingPong.wav");
 			event.getBodyB().setMoveable(false);
-			//model.getWorld().remove(event.getBodyB());
-			//model.removeObjFromBoardAtLocated((int) event.getBodyA().getPosition().getX(), (int) event.getBodyA().getPosition().getY());
-			
 		}
 	}
 
